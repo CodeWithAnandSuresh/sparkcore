eac72572e2b59d04d76cbca0a282e79dd876010e
Anand Suresh <133847835+LearnWithAnandSuresh@users.noreply.github.com>
2023-07-04 11:27:12 +0530

    Create 5_filter_names.py

---
+++ 5_filter_names.py
@@ -0,0 +1,27 @@
+"""
+Question:
+Given a list of names, create a new list that contains only the names starting with the letter 'S'.
+
+Input:
+["John", "Sarah", "Steven", "Emily", "Sam", "Sophia"]
+
+Expected Output:
+["Sarah", "Steven", "Sam", "Sophia"]
+"""
+# Initializing SparkSession and SparkContext objects
+
+from pyspark.sql.session import SparkSession
+spark = SparkSession.builder.master("local").appName("Counting words").getOrCreate()
+sc = spark.sparkContext
+
+# creating a python list with list of values
+lst = ["John", "Sarah", "Steven", "Emily", "Sam", "Sophia"]
+
+# Converting the python list into RDD using parallelize function
+rdd1 = sc.parallelize(lst)
+
+# Filtering out the values that starts with the letter 'S'
+rdd2 = rdd1.filter(lambda x: x[0][0] == 'S')
+
+# Printing only the filtered names from the RDD
+print(f"The list of names starts with 'S' are: {rdd2.collect()}")
