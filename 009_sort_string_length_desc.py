8ab60fc3f71874c5d4f954e99f74ed82daa7b87f
Anand Suresh <133847835+CodeWithAnandSuresh@users.noreply.github.com>
2023-07-13 11:03:06 +0530

    Create 009_sort_string_length_desc.py

---
+++ 009_sort_string_length_desc.py
@@ -0,0 +1,24 @@
+"""
+Question: Given a list of strings, sort the strings in descending order based on their lengths.
+
+Input: ["apple", "banana", "kiwi", "strawberry", "orange"]
+
+Expected Output: ["strawberry", "banana", "orange", "apple", "kiwi"]
+"""
+
+# Initializing SparkSession and SparkContext objects
+from pyspark.sql.session import SparkSession
+spark = SparkSession.builder.master("local").appName("Counting words").getOrCreate()
+sc = spark.sparkContext
+
+# Creating a Python list with the list of string.
+lst = ["apple", "banana", "kiwi", "strawberry", "orange"]
+
+# Converting the Python list to RDD using parallelize function
+rdd1 = sc.parallelize(lst)
+
+# Sorting the RDD with the descending order using the sortBy function with descending order
+rdd2 = rdd1.sortBy(lambda x:len(x),ascending=False)
+
+#Display the sorted list in the descending order
+print(f"The list of strings in descending order: {rdd2}")
