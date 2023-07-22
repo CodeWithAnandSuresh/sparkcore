8e12f2925b3be465aaec3eb4589a9b119055b35c
Anand Suresh <133847835+CodeWithAnandSuresh@users.noreply.github.com>
2023-07-22 21:05:10 +0530

    Update 003_students_avg_score.py

--- 003_students_avg_score.py
+++ 003_students_avg_score.py
@@ -13,7 +13,7 @@ Expected Output: Average score: 86.25
 # Initializing SparkSession and SparkContext objects
 
 from pyspark.sql.session import SparkSession
-spark = SparkSession.builder.master("local").appName("Counting words").getOrCreate()
+spark = SparkSession.builder.master("local").appName("Calculating Student Average Score").getOrCreate()
 sc = spark.sparkContext
 
 # Reading the file, and converting it to RDD using textFile function
